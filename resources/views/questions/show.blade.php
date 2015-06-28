0e0f807459c049b6e46d2c409e637f7056974d18
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-06-28 21:39:32 +0500

    Some modification in the show view

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -13,7 +13,18 @@
 				<div class="col-md-11">
 					<h2>{{ $question->title }}</h2>
 					<hr>
-					<p>{!! $question->description !!}</p>
+					<div>{!! $question->description !!}</div>
+					<div>
+						<a class="label label-primary">pronunciation</a>
+						<a class="label label-primary">name</a>
+						<a class="label label-primary">personal</a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 col-md-offset-9 well">
+					Asked {{ $question->created_at->diffForHumans() }} <br>
+					{{ $question->user->name }}
 				</div>
 			</div>
 		</div>

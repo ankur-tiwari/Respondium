9ba667ae64fc0ffb2bf37abd0664198757248c24
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-07-20 21:22:06 +0500

    Added guzzle

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Mail;
 use App\Http\Requests;
-use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
@@ -16,6 +17,10 @@ class ContactController extends Controller
 
 	public function send(ContactRequest $request)
 	{
+		Mail::raw('Hello World! This is a simple raw email.', function ($message) {
+		    $message->from('no-reply@answersvidsupport.com', 'Rana Faiz Ahmad');
 
+		    $message->to('iamfaizahmed123@gmail.com');
+		});
 	}
 }

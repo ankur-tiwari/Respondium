f76c70585ba9ba00e209c8d9f9e19290231d263a
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-08-15 01:02:17 +0500

    Removed Lorem Ipsum from intro text

--- resources/views/questions/partials/intro.blade.php
+++ resources/views/questions/partials/intro.blade.php
@@ -1,10 +1,9 @@
 <div class="well">
 	<h1 class="intro-title">AnswersVid</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quasi, recusandae aspernatur quam placeat consectetur voluptatem culpa maiores, natus, debitis cupiditate vel non enim molestiae ad molestias tempora temporibus eius.</p>
 	@if (!Auth::check())
-	<div>
-		<a href="/signup" class="btn btn-primary">Signup now</a>
-		<a href="/ask" class="btn btn-primary">Ask a question</a>
-	</div>
+		<div>
+			<a href="/signup" class="btn btn-primary">Signup now</a>
+			<a href="/ask" class="btn btn-primary">Ask a question</a>
+		</div>
 	@endif
 </div>

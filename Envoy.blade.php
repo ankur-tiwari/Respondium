a9207725c0a07ac01dccc1112460ac493ca325d7
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-08-14 23:26:34 +0500

    Update Envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,8 +2,10 @@
 
 @task('deploy', ['on' => 'deploy'])
 	cd /var/www/html/
+	php artisan down
 	git pull
 	composer update
 	php artisan migrate
 	php artisan migrate:testing
+	php artisan up
 @endtask

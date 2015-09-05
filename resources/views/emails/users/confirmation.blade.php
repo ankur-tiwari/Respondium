fe9fbd989ca6e6c5526f2c28111de1ac32770c34
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-09-05 16:52:51 +0500

    Use url() helper in emails

--- resources/views/emails/users/confirmation.blade.php
+++ resources/views/emails/users/confirmation.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-	Thank you very much for registering. Please click at the link below to activate your account. If you have any questions feel free to ask them via <a href="http://lamp.droplet/contact-us">our contact form</a>
+	Thank you very much for registering. Please click at the link below to activate your account. If you have any questions feel free to ask them via <a href="{{ url('/contact-us') }}">our contact form</a>
 	<br>
-	<strong><a href="http://localhost/email/confirm/{{ $code }}">{{ $code }}</a></strong>
+	<strong><a href="{{ url('/email/confirm/' + $code) }}">{{ $code }}</a></strong>
 @stop

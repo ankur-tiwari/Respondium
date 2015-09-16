55c06c51ff472eea3afec5a9bf7c431766f3c8e8
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-09-16 21:53:55 +0500

    Add DigitalOcean link to the footer.

--- resources/views/layouts/partials/front/footer.blade.php
+++ resources/views/layouts/partials/front/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer text-center">
-	<h6 class="copyright-notice">&copy; {{ config('app.name') }} 2015. All rights reserved.</h6>
+	<p><strong>Proudly hosted by <a href="https://www.digitalocean.com/?refcode=5b650acf5d3f">DigitalOcean</a>.</strong></p>
 </footer>

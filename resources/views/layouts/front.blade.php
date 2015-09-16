58c73d939b6875dcc7406c314b67f3326fec0945
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-09-16 22:14:54 +0500

    Add Keywords and Descriptions for search engines

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="videos, questions, web development, programming, Q/A, responses, respondium">
+		<meta name="description", content="Respondium - Get video responses for your web development questions!">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		@include('layouts.partials.front.title')
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">

1419196d288b52486e2228b8b78a7530c205020a
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-10-09 23:56:33 +0500

    Some Change

--- resources/views/layouts/partials/front/title.blade.php
+++ resources/views/layouts/partials/front/title.blade.php
@@ -1,7 +1,7 @@
-@if ( isset($question) and Request::path() !== '/' )
-	<title>{{ $question->title }} - {{ config('app.name') }}</title>
-@elseif ( isset($title) )
+@if ( isset($title) )
 	<title>{{ $title }} - {{ config('app.name') }}</title>
+@elseif ( isset($question) and Request::path() !== '/' )
+	<title>{{ $question->title }} - {{ config('app.name') }}</title>
 @else
 	<title>{{ config('app.name') }}</title>
 @endif

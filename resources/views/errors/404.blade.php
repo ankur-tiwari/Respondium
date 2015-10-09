6132c25a91e08b900b52c9582325069a8fddb4f2
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-10-09 20:26:16 +0500

    Add 404 File

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front')
+
+@section('content')
+	<h1 class="text-center">404: Page Not Found</h1>
+	<hr>
+	<p class="text-center">
+		<a href="/" class="btn btn-default">Go Home</a>
+		<a href="/contact-us" class="btn btn-primary">Contact Support</a>
+	</p>
+@stop

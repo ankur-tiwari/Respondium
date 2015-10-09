111d27b8dff5db41d587300205b1e129b036d273
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-10-09 19:25:00 +0500

    Microdata for questions list

--- resources/views/questions/partials/list.blade.php
+++ resources/views/questions/partials/list.blade.php
@@ -1,10 +1,10 @@
-<li class="list-group-item question-item">
+<li class="list-group-item question-item" itemscope itemtype="http://schema.org/Question">
 	<div class="row">
 		<div class="col-md-12">
 			<h4 class="list-group-item-heading" style="font-weight: bold">
-				<a href="/questions/{{ $question->slug }}">{{ $question->title }}</a>
+				<a itemprop="headline" href="/questions/{{ $question->slug }}">{{ $question->title }}</a>
 			</h4>
-			<p>{{ str_limit($question->description, 400) }}</p>
+			<p itemprop="description">{{ str_limit($question->description, 400) }}</p>
 			<div class="question-tags">
 				@foreach($question->tags as $tag)
 					<a href="/tagged/{{ $tag->name }}" class="label label-default">{{ $tag->name }}</a>
